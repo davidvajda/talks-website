cf83e9f6d290899afa63c510c62cbf05d48784fe
aaa <davidvajda1998@gmail.com>
2022-04-06 21:59:24 +0200

    added style to home page

--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,39 @@
 import React, { useState, useEffect } from "react";
-import "./Chat.css"
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+
+import Toolbar from "@mui/material/Toolbar";
+import Paper from "@mui/material/Paper";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="#">
+        Talks
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
 
 function Chat({ sio, setScreen }) {
   const [text, setText] = useState("");
@@ -8,13 +42,11 @@ function Chat({ sio, setScreen }) {
   // ------ SOCKETIO EVENTS ------
   useEffect(() => {
     sio.on("message", (data) => {
-      console.log(data) // DEBUG CODE
+      console.log(data); // DEBUG CODE
 
       setChatMessages((prevState) => {
-          data.key = prevState.length;
-        return [
-          data, ...prevState,
-        ]
+        data.key = prevState.length;
+        return [data, ...prevState];
       });
     });
 
@@ -52,24 +84,49 @@ function Chat({ sio, setScreen }) {
   };
 
   return (
-    <div>
-      <div>
-        <textarea
-          onChange={(e) => {
-            setText(e.target.value);
-          }}
-        ></textarea>
-        <button onClick={() => sendMessage()}>SEND</button>
-      </div>
-      <div>
-        <h3>CHAT MESSAGES:</h3>
-      </div>
-      <div>
-        <ul>{renderMessages()}</ul>
-      </div>
-      {JSON.stringify(chatMessages)}
-      {chatMessages.length}
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar
+        position="absolute"
+        color="default"
+        elevation={0}
+        sx={{
+          position: "relative",
+          borderBottom: (t) => `1px solid ${t.palette.divider}`,
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h5" color="inherit" noWrap>
+            Company name
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container component="main" maxWidth="sm" sx={{ mb: 4, backgroundSize: "cover",
+            backgroundPosition: "center"}} >
+        <Paper
+          variant="outlined"
+          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+        >
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              ANOTHERS CLIENT NAME
+            </Typography>
+          </Toolbar>
+          <p>bitch</p>
+          <p>bitch</p>
+          <p>bitch</p>
+          <p>bitch</p>
+          <p>bitch</p>
+          <TextField
+              margin="normal"
+              fullWidth
+              id="name"
+              name="text"
+              autoFocus
+            />        </Paper>
+        <Copyright />
+      </Container>
+    </ThemeProvider>
   );
 }
 
